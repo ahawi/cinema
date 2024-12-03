47fc1b2ddd86863d555a75471db3690844c23b75
ultrasue <nastya.murlla@gmail.com>
2024-12-03 11:47:07 +0300

    add none for loader and button

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const apiKey = "e3eead13-89e4-4093-ac45-66ec3298bdc1";
+// настройки
 
+const apiKey = "e3eead13-89e4-4093-ac45-66ec3298bdc1";
 const url = "https://kinopoiskapiunofficial.tech/api/v2.2/films/";
 const options = {
   method: "GET",
@@ -9,49 +10,50 @@ const options = {
   },
 };
 
+// DOM-элементы
 const filmsWrapper = document.querySelector(".films");
+const loader = document.querySelector(".loader-wrapper");
+const buttonLoadMore = document.querySelector(".show-more");
 
-// через промисы
-// fetch(`${url}top`, options)
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-//   .catch((err) => console.log(err));
-
-// через async await
+// получение и вывод ТОП 250 фильмов
 async function fetchAndRenderFilms() {
-  const response = await fetch(`${url}top`, options);
-  const data = await response.json();
-  console.log(data);
-  console.log(data.films);
+  //показать прелоадер
+  loader.classList.remove("none");
 
-  for (film of data.films) {
-    console.log(film);
+  //получаем фильмы
+  const data = await fetchData(`${url}top`, options);
+
+  //проверка на доп страницы
+  if (data.pagesCount > 1) {
+    //отобразить кнопку
+    buttonLoadMore.classList.remove("none");
+  }
+
+  //скрываем прелоадер
+  loader.classList.add("none");
+
+  //рендерим фильмы на страницу
+  renderFilms(data.films);
+}
+
+async function fetchData(url, options) {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+}
+
+function renderFilms(films) {
+  for (film of films) {
     const html = `<div class="films">
-        <div class="card">
-          <img src=${film.posterUrlPreview} alt="Cover" class="card__img" />
-          <h3 class="card__title">${film.nameRu}</h3>
-          <p class="card__year">${film.year}</p>
-          <p class="card__rate">Рейтинг: ${film.rating}</p>
-        </div>`;
+            <div class="card">
+              <img src=${film.posterUrlPreview} alt="Cover" class="card__img" />
+              <h3 class="card__title">${film.nameRu}</h3>
+              <p class="card__year">${film.year}</p>
+              <p class="card__rate">Рейтинг: ${film.rating}</p>
+            </div>`;
 
     filmsWrapper.insertAdjacentHTML("beforeend", html);
   }
 }
 
 fetchAndRenderFilms().catch((err) => console.log(err));
-
-// способы вызвать catch
-//
-// fetchAndRenderFilms().catch((err) => console.log(err))
-//
-// или
-//
-// async function fetchAndRenderFilms() {
-//   try {
-//     const response = await fetch(`${url}top`, options);
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
